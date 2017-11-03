5b7d501caea26b7c7c293efb9d6c9d6f62e7f60b
Remmy Clay <remmy.lauren@gmail.com>
2017-11-03 07:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function createRock(x) {
 
   function moveRock(rock) {
     rock.style.top = `${top += 2}px`;
-    if (checkCollision) {
+    if (checkCollision(rock)) {
       endGame();
     } if (top < 380) {
       window.requestAnimationFrame(moveRock);
